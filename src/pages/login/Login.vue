be4c42635fd336e212f218cda00c36a8c2f4edaf
Clover996 <clover113@163.com>
2021-12-03 19:17:25 +0800

    new pages

---
+++ src/pages/login/Login.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- <router-link></router-link> -->
+  <!-- 跳转 -->
+  <div>Login</div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style>
+
+</style>
